9d8e68084159a609db2312b66d82861ffdf60d39
Ramsey Shehadeh <ramseys@doodleplex.com>
2022-12-22 18:51:31 -0500

    Show lyrics/no lyrics quick query on the songs page.

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,6 +15,7 @@ class Song < ApplicationRecord
     QuickQuery.new('songs', :not_written_by_robyn),
     QuickQuery.new('songs', :never_released, [:originals, :covers]),
     QuickQuery.new('songs', :has_guitar_tabs, [:no_tabs]),
+    QuickQuery.new('songs', :has_lyrics, [:no_lyrics]),
     QuickQuery.new('songs', :improvised),
     QuickQuery.new('songs', :released_never_played_live)
   ]
