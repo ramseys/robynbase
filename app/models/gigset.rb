2266fe35f144238e682f0a0d130cf6bcb7f5c5a5
Ramsey Shehadeh <ramseys@doodleplex.com>
2026-06-09 19:30:44 -0400

    Add comments

--- app/models/gigset.rb
+++ app/models/gigset.rb
@@ -4,6 +4,7 @@ class Gigset < ApplicationRecord
   belongs_to :gig, foreign_key: "GIGID", inverse_of: :gigsets
   belongs_to :song, foreign_key: "SONGID", optional: true
 
+  # using the numericality check because a Gigset.new will default to 0 if SONGID is not specified
   validates :SONGID, numericality: { greater_than: 0 }, unless: -> { self[:Song].present? }
 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -4,6 +4,7 @@ class Track < ApplicationRecord
   belongs_to :composition, foreign_key: "COMPID", inverse_of: :tracks
   belongs_to :song, foreign_key: "SONGID", optional: true
 
+  # using the numericality check because a Track.new will default to 0 if SONGID is not specified
   validates :SONGID, numericality: { greater_than: 0 }, unless: -> { self[:Song].present? }
 
 end
